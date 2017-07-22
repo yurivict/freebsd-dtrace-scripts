d836d1f6bcd422041444bf6f72e8bc98b8f1d7ef
Yuri <yuri@tsoft.com>
2017-07-22 10:40:47 -0700

    Added process-execution.d

---
+++ process-execution.d
@@ -0,0 +1,23 @@
+#!/usr/sbin/dtrace -s
+
+/*
+ * Snoop Process Execution
+ * {rints details on new processes as they execute
+ *
+ * from the book: Brendan Gregg, Dynamic Tracing in Oracle® Solaris, Mac OS X, and FreeBSD
+ */
+
+#pragma D option quiet
+#pragma D option switchrate=10hz
+
+dtrace:::BEGIN
+{
+  printf("%-20s %6s %6s %6s %s\n", "ENDTIME",
+    "UID", "PPID", "PID", "PROCESS");
+}
+proc:::exec-success
+{
+   printf("%-20Y %6d %6d %6d %s\n", walltimestamp,
+   uid, ppid, pid, execname);
+}
+
