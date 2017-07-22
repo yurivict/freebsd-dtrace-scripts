d842080c607e93b5accd467a803c1be11d80cacc
Yuri <yuri@tsoft.com>
2017-07-22 16:14:43 -0700

    Added 'openat'

--- open-file.d
+++ open-file.d
@@ -13,6 +13,18 @@ syscall::open:entry /execname == $$1/ {
 }
 
 syscall::open:return /execname == $$1/ {
-  printf("file=%s flags=%x errno=%d @tm=%d dt=%d pid=%d", copyinstr(self->file), self->flags, errno, timestamp, timestamp-self->tm, pid);
+  printf("file=%s flags=%x errno=%d return-fd=%d @tm=%d dt=%d pid=%d", copyinstr(self->file), self->flags, errno, arg0, timestamp, timestamp-self->tm, pid);
+  ustack();
+}
+
+syscall::openat:entry /execname == $$1/ {
+  self->tm = timestamp;
+  printf("file=%s flags=%x at-fd=%d @tm=%d pid=%d", copyinstr(arg1), arg2, arg0, self->tm, pid);
+  self->file = arg1;
+  self->flags = arg2;
+}
+
+syscall::openat:return /execname == $$1/ {
+  printf("file=%s flags=%x errno=%d return-fd=%d @tm=%d dt=%d pid=%d", copyinstr(self->file), self->flags, errno, arg0, timestamp, timestamp-self->tm, pid);
   /*ustack();*/
 }
