150614c3e08f0c393c430835bff1056ca98b587f
Yuri <yuri@rawbw.com>
2019-05-26 15:51:40 -0700

    Improve process-call-inner-symbol.d

--- process-call-inner-symbol.d
+++ process-call-inner-symbol.d
@@ -1,11 +1,17 @@
 #!/usr/sbin/dtrace -s
 
 /*
- * Trace internal functions in a process with a given pid
+ * Trace internal functions in a process with a given pid (also works with symbols imported into the process from shared libraries)
  * Args: pid
  *       function-name
  */
 
 pid$1::$$2:entry {
+  printf("pid=%d symbol=%s", $1, $$2);
+  ustack();
+}
+
+pid$1::$$2:return {
+  printf("pid=%d symbol=%s arg0=%d", $1, $$2, arg0);
   ustack();
 }
